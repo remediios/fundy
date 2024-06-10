06466e2275218f5510cbd9fa9fc5bdd2cca757d0
Miguel Remédios <miguelremediios@gmail.com>
2024-06-10 23:20:05 +0100

    feat: add userbutton for logout option

--- app/(dashboard)/page.tsx
+++ app/(dashboard)/page.tsx
@@ -1,5 +1,11 @@
 import { Button } from '@/components/ui/button';
+import { UserButton } from '@clerk/nextjs';
 
 export default function Home() {
-  return <p>This is an authenticated route</p>;
+  return (
+    <div>
+      <UserButton afterSignOutUrl="/" />
+      <p>This is an authenticated route</p>
+    </div>
+  );
 }
