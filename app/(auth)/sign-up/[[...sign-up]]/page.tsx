b5fe0ffb0f5ed3c4e17b397c51874748391aae84
Miguel Remédios <miguelremediios@gmail.com>
2024-06-10 23:07:24 +0100

    feat: finish signin and signup authentication pages

--- app/(auth)/sign-up/[[...sign-up]]/page.tsx
+++ app/(auth)/sign-up/[[...sign-up]]/page.tsx
@@ -1,5 +1,31 @@
-import { SignUp } from '@clerk/nextjs';
+import Image from 'next/image';
+import { Loader2 } from 'lucide-react';
+import { ClerkLoaded, ClerkLoading, SignUp } from '@clerk/nextjs';
 
 export default function Page() {
-  return <SignUp path="/sign-up" />;
+  return (
+    <div className="min-h-screen grid grid-cols-1 lg:grid-cols-2">
+      <div className="h-full lg:flex flex-col items-center justify-center px-4">
+        <div className="text-center space-y-4 pt-16">
+          <h1 className="font-bold text-3xl text-[#2E2A47]">
+            Register yourself!
+          </h1>
+          <p className="text-base text-[#7E8CA0]">
+            Create your account to access the platform!
+          </p>
+        </div>
+        <div className="flex items-center justify-center mt-8">
+          <ClerkLoaded>
+            <SignUp path="/sign-up" />
+          </ClerkLoaded>
+          <ClerkLoading>
+            <Loader2 className="animate-spin text-muted-foreground" />
+          </ClerkLoading>
+        </div>
+      </div>
+      <div className="h-full bg-red-400 hidden lg:flex items-center justify-center">
+        <Image src="/logo_white.svg" height={150} width={150} alt="Logo" />
+      </div>
+    </div>
+  );
 }
