e659a05db3bf8ffe7b1a1055d0309295d36326e5
Miguel Remédios <miguelremediios@gmail.com>
2024-06-14 23:12:55 +0100

    feat: hono RPC and  method chaining

--- app/api/[[...route]]/route.ts
+++ app/api/[[...route]]/route.ts
@@ -1,15 +1,19 @@
 import { Hono } from 'hono';
 import { handle } from 'hono/vercel';
+import { clerkMiddleware, getAuth } from '@hono/clerk-auth';
+import authors from './authors';
 
 export const runtime = 'edge';
 
 const app = new Hono().basePath('/api');
 
-app.get('/hello', (c) => {
+app.get('/status', (c) => {
   return c.json({
-    message: 'Hello Next.js!',
+    message: '[FUNDY]: API IS OK',
   });
 });
+
+app.route('/authors', authors);
 
 export const GET = handle(app);
 export const POST = handle(app);
